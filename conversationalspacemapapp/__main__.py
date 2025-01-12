44c642b450965f6af4ed842df3f5db4a19f7b1dd
Manuel Bieri <mail@manuelbieri.ch>
2025-01-12 11:01:53 +0100

    Remove PyQt6 dependency.

--- conversationalspacemapapp/__main__.py
+++ conversationalspacemapapp/__main__.py
@@ -1,16 +1,10 @@
 import conversationalspacemapapp.App.TogaApp.app as togaApp
-import conversationalspacemapapp.App.QtApp.app as QtApp
 
 def main(toga=True):
     if toga:
         togaApp.main().main_loop()
     else:
-        import sys
-        from PyQt6.QtWidgets import QApplication
-        app = QApplication(sys.argv)
-        window = QtApp.main()
-        window.show()
-        sys.exit(app.exec())
+        NotImplemented("No other GUI implementation available.")
 
 if __name__ == "__main__":
     main()
